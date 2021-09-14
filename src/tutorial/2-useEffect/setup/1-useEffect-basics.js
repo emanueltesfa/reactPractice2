5ad4b143f5e2f106bd7b469fd642aed0756f95c4
tabraha <amant@tabrahas-MacBook-Pro.local>
2021-09-14 00:13:14 -0400

    Inital Commit

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [text, setText] = useState("Random Title");
+
+  const handleClick = () => {
+    if (text === "Random title") {
+      setText("Hello World");
+    } else {
+      setText("Random title");
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseEffectBasics;
